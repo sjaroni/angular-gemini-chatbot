6a545b76d77b87815367b9f0c358b8a68c8c3e06
Stefan Jaroni <sjaroni@outlook.de>
2025-03-26 22:17:52 +0100

    style: questions and answers, add cdn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,44 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 import { GeminiService } from './gemini.service';
-import { SkeletonComponent } from "./skeleton/skeleton.component";
+import { SkeletonComponent } from './skeleton/skeleton.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SkeletonComponent, FormsModule],
+  imports: [SkeletonComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-
   title = 'angular-gemini-chatbot';
   prompt: string = '';
   geminiService: GeminiService = inject(GeminiService);
+  chatHistory: any[] = [];
+  loading: boolean = false;
 
-  sendData() {
-    if(this.prompt) {      
-      this.geminiService.generateText(this.prompt);
-      console.log(this.prompt);      
+  constructor() {
+    this.geminiService.getMessageHistory().subscribe((res) => {
+      if (res) {
+        this.chatHistory.push(res);
+      }
+    });
+  }
+
+  async sendData() {
+    if (this.prompt) {
+      this.loading = true;
+      const data = this.prompt;
+      this.prompt = '';
+      await this.geminiService.generateText(data);
+      this.loading = false;
     }
+  }
+
+  formatText(text: string) {
+    const result = text.replaceAll('*', '');
+    return result;
   }
 
 }
