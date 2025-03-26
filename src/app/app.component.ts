180aa963d9905d579015677bff466631a013d422
Stefan Jaroni <sjaroni@outlook.de>
2025-03-26 19:29:53 +0100

    style: add simple layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkeletonComponent } from "./skeleton/skeleton.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SkeletonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
