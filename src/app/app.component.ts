391f4d9e7e3cc21c6750440e5af8f04ed9672445
Stefan Jaroni <sjaroni@outlook.de>
2025-03-26 21:11:27 +0100

    feat: implement gemini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { GeminiService } from './gemini.service';
 import { SkeletonComponent } from "./skeleton/skeleton.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SkeletonComponent],
+  standalone: true,
+  imports: [RouterOutlet, SkeletonComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
   title = 'angular-gemini-chatbot';
+  prompt: string = '';
+  geminiService: GeminiService = inject(GeminiService);
+
+  sendData() {
+    if(this.prompt) {      
+      this.geminiService.generateText(this.prompt);
+      console.log(this.prompt);      
+    }
+  }
+
 }
